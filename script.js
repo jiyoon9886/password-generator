8f7fad13446a9b959c78372baf1a51a9018d5bb6
Jiyoon Kim <jiyoon9886@gmail.com>
2020-04-03 02:09:01 -0400

    created random character generator functions

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
  
 //What criteria does the user want
-var userLength = prompt ("Enter a length for desired password. (At least 8 characters and no more than 128 characters)");
+/* var userLength = prompt ("Enter a length for desired password. (At least 8 characters and no more than 128 characters)");
 var includeLowercase = confirm ("Would you like to include lowercase letters?");
 var includeUppercase = confirm ("Would you like to include uppercase letters?");
 var includeNumbers = confirm ("Would you like to include numbers?");
-var includeSpecialChar = confirm ("Would you like to include special characters?");
+var includeSpecialChars = confirm ("Would you like to include symbols?"); */
+
+//Random character generators-http://www.net-comber.com/charset.html 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+
+function getRandomSymbol() {
+  var symbols = ["!", "#", "$", "%", "^", "&", "(", ")", "*", "+", "-", ".", "/", "<", "=", ">", "?", "@", "_", "~"];
+  return symbols[(Math.floor(Math.random() *20))];
+}
 
 
-//console.log (includeLowercase);
+console.log(getRandomSymbol());
+
 
 /* function generatePassword() {
-  var length = ;
+  var length = userLength;
   charset
 
 } */
